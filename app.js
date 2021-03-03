5b7c3146263b834d969d1d4b247d2ff059d424c5
Samir Sharma <samirsharma.iitd@gmail.com>
2021-03-03 23:51:42 +0530

    Clean up code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-alert("Keep API-requests limited!");
+alert("Keep API-requests limited!");//Even a VPN doesn't work!
 
 // Get the GitHub input form
 const gitHubForm = document.getElementById("gitHubForm");
@@ -7,46 +7,69 @@ const gitHubForm = document.getElementById("gitHubForm");
 gitHubForm.addEventListener("submit", (e) => {
   // Prevent default form submission action
   e.preventDefault();
+
   // Get the GitHub username input field on the DOM
   let usernameInput = document.getElementById("usernameInput");
   // Get the value of the GitHub username input field
   let gitHubUsername = usernameInput.value;
+
+  // Get the GitHub Repo_count(n) input field on the DOM
   let Repo_count = document.getElementById("Repo_count");
+  // Get the value of the Repo_count username input field
   let n = Repo_count.value;
+
+  // Get the GitHub Commitee_count(m) input field on the DOM
   let Commitee_count = document.getElementById("Commitee_count");
+  // Get the value of the Commitee_count username input field
   let m = Commitee_count.value;
+
+
   // Run GitHub API function, passing in the GitHub username
   requestUserRepos(gitHubUsername, n, m);
-});
+} );
 
 function requestUserRepos(username, n, m) {
-  // Create new XMLHttpRequest object
+
+  //Counter variable for Repo_count(n)
   c = 0;
-  // GitHub endpoint, dynamically passing in specified username
-  for (let j = 1; j <= 10; j++) {
+
+  //API request limit(20) to 2000 repositories per organisation..Can be increased.
+  for (let j = 1; j <= 20; j++) {
+    
+    // Create new XMLHttpRequest object
     xhr = new XMLHttpRequest();
-    // url = `https://api.github.com/users/${username}/repos?per_page=100&page=${j}`;
+
+    // GitHub endpoint, dynamically passing in specified username and page number.
     url = `https://api.github.com/search/repositories?o=desc&q=${username}&s=forks&per_page=100&page=${j}`;
+
     // Open a new connection, using a GET request via URL endpoint
     // Providing 3 arguments (GET/POST, The URL, Async True/False)
     xhr.open("GET", url, true);
+
     // When request is received
     // Process it here
     xhr.onload = function () {
+
       // Parse API data into JSON
       const data = JSON.parse(this.response);
+
       // Loop over each object in data array
       for (let i in data) {
+        
         c++;
         if (c > n) {
           break;
         }
+        
         // Get the ul with id of of userRepos
         let ul = document.getElementById("userRepos");
+        
         // Create variable that will create li's to be added to ul
         let li = document.createElement("li");
+        
         // Add Bootstrap list item class to each li
         li.classList.add("list-group-item");
+        
         // Create the html markup for each li
         li.innerHTML = `
                     <p><strong>Repo:</strong> ${data[i].name}</p>
@@ -55,10 +78,13 @@ function requestUserRepos(username, n, m) {
                     <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
                     <ul id="contributers_list">Top-Contributers</ul>
                 `;
+
+        //Add commitees and commit_count
         fetchContributers(username,data[i].name,m)
+
         // Append each li to the ul
         ul.appendChild(li);
-        // console.log(c);
+        // console.log(c);//Enable for debugging.
       }
     };
     // Send the request to the server
@@ -67,24 +93,35 @@ function requestUserRepos(username, n, m) {
 }
 
 function fetchContributers(username,repo_name,m){
+    //Counter variable for Commitee_count(m)
+    c=0;
+
+    //New XML Request Object
     xhr = new XMLHttpRequest();
-    // https://api.github.com/repos/google/googletest/contributors?per_page=100
+    //Endpoint(Auto-sorted by the API,yay!)
     url=`https://api.github.com/repos/${username}/${repo_name}/contributers?per_page=100`
+
     xhr.open("GET", url, true);
     xhr.onload = function () {
+
         // Parse API data into JSON
         const data = JSON.parse(this.response);
         for (let i in data) {
+
             c++;
             if (c > m) {
             break;
             }
+
             // Get the ul with id of of userRepos
             let ul = document.getElementById("contributers_list");
+            
             // Create variable that will create li's to be added to ul
             let li = document.createElement("li");
+
             // Add Bootstrap list item class to each li
             li.classList.add("list-group-item");
+
             // Create the html markup for each li
             li.innerHTML = `<div>${data[i].login}<div>${data[i].contributions}</div></div>`
             ul.appendChild(li);
